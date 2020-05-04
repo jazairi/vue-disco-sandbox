e1d81db8036558649db28e674b6287f979521bb7
Adam Jazairi <jazairi@mit.edu>
2020-05-04 12:03:36 -0400

    Add Header and Search components

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="header">
+    <h1>Distinctive Collections Discovery</h1>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    props: ['title'],
+    setup() {}
+  }
