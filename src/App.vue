f3b2688b84ab3a5a749420b85eb44e4987c363cc
Adam Jazairi <jazairi@mit.edu>
2020-05-04 12:43:56 -0400

    Add search functionality to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <Header title="Distinctive Collections Discovery" />
-
+    <img alt="Vue logo" src="./assets/libraries-logo.jpg" />
+    <Header v-bind:title="'Distinctive Collections Discovery'" />
+    <Search v-bind:search="state.search" v-on:search="handleSearch" />
   </div>
 </template>
 
 <script>
+  import { reactive, watch } from '@vue/composition-api';
+  import Header from './components/Header.vue';
+  import Search from './components/Search.vue';
+  import Record from './components/Record.vue';
+
   export default {
     name: "App",
     components: {
+      Header, Search, Record
+    },
+    setup() {
+      const state = reactive({
+        term: 'popcorn',
+        loading: true,
+        searchResults: [],
+        errorMessage: null
+      });
+
+      watch(() => {
+        const TIMDEX_API_URL = `'https://timdex.mit.edu/api/v1/search?q=${state.term}`;
+
+        fetch(TIMDEX_API_URL)
+          .then(response => response.json())
+          .then(data => {
+            state.searchResults = data.Search;
+            state.loading = false;
+          });
+      });
+
+      return {
+        state,
+        handleSearch(searchTerm) {
+          state.loading = true;
+          state.search = searchTerm;
+        }
+      }
     }
-  };
+  }
 </script>
 
 <style>
---
+++ src/components/Record.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="record">
+    <h2>{{ record.title }}</h2>
+    <p>{{ record.sourceLink }}</p>
+  </div>
+</template>
+
+<script>
+  import { computed } from '@vue/composition-api';
+
+  export default {
+    name: "Record",
+    props: ['record'],
+    setup({ record }) {
+      const altText = computed(() => `The record title ${record.title}`);
+      return altText;
+    }
+  }
+</script>
